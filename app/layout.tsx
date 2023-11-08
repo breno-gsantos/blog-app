64441882a21481ad19739838409c69991c2cc859
Breno <brenogiovanniolintho@gmail.com>
2023-11-08 11:33:12 -0300

    Project Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter, Manrope } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-in' })
+const manrope = Manrope({ subsets: ['latin'], display: 'swap', variable: '--font-mr' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${manrope.variable} font-mr bg-light dark:bg-dark`}>{children}</body>
     </html>
   )
 }
